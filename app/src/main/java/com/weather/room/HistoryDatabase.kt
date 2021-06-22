6645460927e470a322e9ee81163d0ba2e3726664
Verruckte <Soulkillerdeath@gmail.com>
2021-06-22 15:08:12 +0300

    Weather DZ9

--- app/src/main/java/com/weather/room/HistoryDatabase.kt
+++ app/src/main/java/com/weather/room/HistoryDatabase.kt
@@ -3,7 +3,7 @@ package com.weather.room
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [HistoryEntity::class], version = 1, exportSchema = false)
+@Database(entities = arrayOf(HistoryEntity::class), version = 1, exportSchema = false)
 abstract class HistoryDatabase : RoomDatabase() {
 
     abstract fun historyDao(): HistoryDao
